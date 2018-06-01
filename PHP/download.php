e503d4b1bf98760325c86c5a44d66ce59364eda9
Unknown <D257443@edu.rocwb.nl>
2018-06-01 10:08:52 +0200

    CSV

---
+++ PHP/download.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once('config.php');
+
+$statement = $dbh->prepare("SELECT * FROM tbl_matches");
+ 
+$statement->execute();
+ 
+$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+ 
+$columnNames = array();
+if(!empty($rows)){
+
+    $firstRow = $rows[0];
+    foreach($firstRow as $colName => $val){
+        $columnNames[] = $colName;
+    }
+}
+ 
+$fileName = 'matches.csv';
+ 
+header('Content-Type: application/excel');
+header('Content-Disposition: attachment; filename="' . $fileName . '"');
+ 
+$fp = fopen('php://output', 'w');
+ 
+fputcsv($fp, $columnNames);
+ 
+foreach ($rows as $row) {
+    fputcsv($fp, $row);
+}
+ 
+fclose($fp);
+
+?>
